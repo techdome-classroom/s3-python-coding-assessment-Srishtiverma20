e8a8b6f51a0c21b6a499e8b23d06528a229beb0e
Srishti Verma <155990289+Srishtiverma20@users.noreply.github.com>
2024-10-07 15:50:50 +0530

    program1.py

--- program1.py
+++ program1.py
@@ -1,13 +1,26 @@
-class Solution(object):
-    def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
-        pass
+class Solution:
+    def isValid(self, s: str) -> bool:
+        arr = [''] * len(s)
+        top = -1
 
-
-
+        for ch in s:
+            if ch =='(':
+                top+=1
+                arr[top] = ')'
+            elif ch =='[':
+                top+=1
+                arr[top] = ']'
+            elif ch =='{':
+                top+=1
+                arr[top] = '}'
+            elif top == -1 or arr[top] != ch:
+                return False
+            else:
+                top -= 1
+            
+        
+        # All counts should be zero if balanced
+        return top == -1
 
 
 
