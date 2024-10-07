dd487ce382ad8077f90d093c87ba36e4fe7ae456
Srishti Verma <155990289+Srishtiverma20@users.noreply.github.com>
2024-10-07 15:55:06 +0530

    program1.py

--- program1.py
+++ program1.py
@@ -1,33 +1,18 @@
-class Solution:
-    def isValid(self, s: str) -> bool:
-        arr = [''] * len(s)
-        top = -1
-
-        for ch in s:
-            if ch =='(':
-                top+=1
-                arr[top] = ')'
-            elif ch =='[':
-                top+=1
-                arr[top] = ']'
-            elif ch =='{':
-                top+=1
-                arr[top] = '}'
-            elif top == -1 or arr[top] != ch:
+class Solution(object):
+	def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        d = {'(':')', '{':'}','[':']'}
+        stack = []
+        for i in s:
+            if i in d:  # 1
+                stack.append(i)
+            elif len(stack) == 0 or d[stack.pop()] != i:  # 2
                 return False
-            else:
-                top -= 1
-            
-        
-        # All counts should be zero if balanced
-        return top == -1
-
-
-
-
-    
-
-
-
-  
-
+        return len(stack) == 0 # 3
+	
+# 1. if it's the left bracket then we append it to the stack
+# 2. else if it's the right bracket and the stack is empty(meaning no matching left bracket), or the left bracket doesn't match
+# 3. finally check if the stack still contains unmatched left bracket
